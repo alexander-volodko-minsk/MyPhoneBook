9d9d8506fd31d8c1532668fee61de85ab8077381
Aliaksandr Valadzko <Aliaksandr_Valadzko@epam.com>
2015-12-04 11:03:16 +0300

    second task is ready

---
+++ src/by_epam/task2/dao/memory/Runner.java
@@ -0,0 +1,20 @@
+package by_epam.task2.dao.memory;
+
+import by_epam.task2.entity.Note;
+import by_epam.task2.entity.NoteBook;
+import by_epam.task2.view.NoteBookConsoleView;
+
+public class Runner
+{
+
+	public static void main(String[] args)
+	{
+		NoteBook testNoteBook = NoteBookProvider.getInstance().getNotebook();//создаем тестовый NoteBook
+		Note testNote = new Note("testMessage");//создаем тестовую запись Note
+		testNoteBook.getNotes().add(testNote);//добавляем запись в книгу
+		NoteBookConsoleView testNoteBookConsoleView = new NoteBookConsoleView(testNoteBook);//создаем тестовый NoteBookConsoleView
+		testNoteBookConsoleView.print();//должны увидеть сообщение о выводе всех записей на экран и тестовую запись
+		NoteBookProvider.getInstance();//проверяем правильность Singleton, должны увидеть сообщение об ошибке
+	}
+
+}
---
+++ src/by_epam/task2/view/NoteBookConsoleView.java
@@ -0,0 +1,48 @@
+package by_epam.task2.view;
+
+import by_epam.task2.entity.Note;
+import by_epam.task2.entity.NoteBook;
+
+/*
+ * Класс NoteBookConsoleView, представляет собой вид нашей программы
+ * содержит перегруженные методы вывода на консоль записей Note нашей записной книги NoteBook
+ */
+
+public class NoteBookConsoleView 
+{
+	//Поле класса
+	private NoteBook noteBookToView;
+	
+    //Коструктор класса, в качестве параметра передается наша записная книга
+	public NoteBookConsoleView(NoteBook noteBookToView)
+	{
+		this.noteBookToView=noteBookToView;
+	}
+	
+	
+	/*
+	 * Перегружаем методы print
+	 * Метод print способен выводить как все записи, так и несколько указанных записей
+	 */
+	//метод print выводящий все записи
+	public void print()
+	{
+		System.out.println("Printing all of the notes");
+		
+		for ( Note noteToView : noteBookToView.getNotes())
+		{
+			System.out.println(noteToView.toString());
+		}
+	}
+	//метод print с переменным числом параметров
+	public void print(Note ... args)
+	{
+		System.out.println("Printing some of the notes");
+		
+		for ( Note noteToView : args)
+		{
+			System.out.println(noteToView.toString());
+		}
+	}
+
+}
